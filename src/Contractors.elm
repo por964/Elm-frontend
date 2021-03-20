31996ed5cb52093d5e570f4dc5df0fef15cae20d
Claes <claesvonh@gmail.com>
2021-03-21 00:19:40 +0100

    first commit

---
+++ src/Contractors.elm
@@ -0,0 +1,196 @@
+module Contractors exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode exposing (Decoder, int, list, string)
+import Json.Decode.Pipeline exposing (required)
+
+
+type alias Post =
+    { id : Int
+    , fName : String
+    , lName : String
+    , zip : String
+    , mail : String
+    }
+
+
+type alias Model =
+    { posts : List Post
+    , errorMessage : Maybe String
+    }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick SendHttpRequest ]
+            [ text "Get data from server" ]
+        , viewPostsOrError model
+        ]
+
+
+viewPostsOrError : Model -> Html Msg
+viewPostsOrError model =
+    case model.errorMessage of
+        Just message ->
+            viewError message
+
+        Nothing ->
+            viewPosts model.posts
+
+
+viewError : String -> Html Msg
+viewError errorMessage =
+    let
+        errorHeading =
+            "Couldn't fetch data at this time."
+    in
+    div []
+        [ h3 [] [ text errorHeading ]
+        , text ("Error: " ++ errorMessage)
+        ]
+
+
+viewPosts : List Post -> Html Msg
+viewPosts posts =
+    div []
+        [ h3 [] [ text "Posts" ]
+        , table []
+            ([ viewTableHeader ] ++ List.map viewPost posts)
+        ]
+
+
+viewTableHeader : Html Msg
+viewTableHeader =
+    tr []
+        [ th []
+            [ text "ID" ]
+        , th []
+            [ text "First" ]
+        , th []
+            [ text "Last" ]
+        , th []
+            [ text "ZIP" ]
+        , th []
+            [ text "Mail" ]
+        ]
+
+
+viewPost : Post -> Html Msg
+viewPost post =
+    tr []
+        [ td []
+            [ text (String.fromInt post.id) ]
+        , td []
+            [ text post.fName ]
+        , td []
+            [ text post.lName ]
+        , td []
+            [ text post.zip ]
+        , td []
+            [ text post.mail ]
+        ]
+
+
+type Msg
+    = SendHttpRequest
+    | DataReceived (Result Http.Error (List Post))
+
+
+postsDecoder : Decode.Decoder (List Post)
+postsDecoder =
+    Decode.list postDecoder
+
+
+postDecoder : Decoder Post
+postDecoder =
+    Decode.succeed Post
+        |> required "id" int
+        |> required "fName" string
+        |> required "lName" string
+        |> required "zip" string
+        |> required "mail" string
+
+
+
+{--
+postDecoder : Decoder Post
+postDecoder =
+    map5 Post
+        (field "id" int)
+        (field "fName" string)
+        (field "lName" string)
+        (field "zip" string)
+        (field "mail" string)
+--}
+
+
+httpCommand : Cmd Msg
+httpCommand =
+    Http.get
+        { url = "http://localhost:8080/org/api/org/con/all"
+        , expect = Http.expectJson DataReceived (list postDecoder)
+        }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SendHttpRequest ->
+            ( model, httpCommand )
+
+        DataReceived (Ok posts) ->
+            ( { model
+                | posts = posts
+                , errorMessage = Nothing
+              }
+            , Cmd.none
+            )
+
+        DataReceived (Err httpError) ->
+            ( { model
+                | errorMessage = Just (buildErrorMessage httpError)
+              }
+            , Cmd.none
+            )
+
+
+buildErrorMessage : Http.Error -> String
+buildErrorMessage httpError =
+    case httpError of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Server is taking too long to respond. Please try again later."
+
+        Http.NetworkError ->
+            "Unable to reach server."
+
+        Http.BadStatus statusCode ->
+            "Request failed with status code: " ++ String.fromInt statusCode
+
+        Http.BadBody message ->
+            message
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { posts = []
+      , errorMessage = Nothing
+      }
+    , Cmd.none
+    )
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
---
+++ src/Counter.elm
@@ -0,0 +1,42 @@
+module Counter exposing (main)
+
+import Browser
+import Html.Events exposing (..)
+import Html exposing (..)
+
+type alias Model =
+    Int
+
+
+initialModel : Model
+initialModel =
+    0
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+                , text (String.fromInt model)
+                , button [ onClick Increment ] [ text "+" ]
+                ]
+
+type Msg
+    = Increment
+    | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
